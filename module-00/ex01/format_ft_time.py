c3be19671781380c6ec7d78056281840e7721a6a
Manuel SHARIFI <manuel.sharifi@gmail.com>
2024-02-22 16:35:16 +0100

    [FIX] format seconds

--- module-00/ex01/format_ft_time.py
+++ module-00/ex01/format_ft_time.py
@@ -3,7 +3,8 @@ import datetime
 
 seconds_since_epoch = time.time()
 scientific_notation = "{:.2e}".format(seconds_since_epoch)
+formated_seconds = "{:,.4f}".format(seconds_since_epoch)
 current_date = datetime.datetime.now().strftime("%b %d %Y")
 
-print("Seconds since January 1, 1970:", scientific_notation, "or", seconds_since_epoch, "in scientific notation")
+print("Seconds since January 1, 1970:", scientific_notation, "or", formated_seconds, "in scientific notation")
 print(current_date)
