b3de4cb8c258bdef5b19e81693e3a02441b01554
Manuel SHARIFI <manuel.sharifi@gmail.com>
2024-02-22 16:58:24 +0100

    [FINISH] ex03

---
+++ module-00/ex03/NULL_not_found.py
@@ -0,0 +1,14 @@
+def NULL_not_found(object: any) -> int:
+	match object:
+		case None:
+			return (print("Nothing : None", type(object)), 0)
+		case float():
+			return (print("Cheese : nan", type(object)), 0)
+		case 0 if not isinstance(object, bool) and isinstance(object, int):
+			return (print("Zero : 0", type(object)), 0)
+		case '':
+			return (print("Empty:", type(object)), 0)
+		case False:
+			return (print("Fake: False", type(object)), 0)
+	return (1)
+  
