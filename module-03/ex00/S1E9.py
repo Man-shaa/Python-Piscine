ac3b87f4fd00f22699dc90bfbc9dc468625d3eb5
Manuel SHARIFI <manuel.sharifi@gmail.com>
2024-05-11 15:49:33 +0200

    [FINISH] ex00

---
+++ module-03/ex00/S1E9.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    """Your docstring for Class"""
+    @abstractmethod
+    def __init__(this, first_name, is_alive=True):
+        this.first_name = first_name
+        this.is_alive = is_alive
+    def die(this):
+        """Your docstring for Method die."""
+        this.is_alive = False
+
+
+class Stark(Character):
+    """Your docstring for Class"""
+    def __init__(this, first_name, is_alive=True):
+        """Your docstring for Constructor."""
+        super().__init__(first_name, is_alive)
+
+
+def main():
+    #your code here
+    Ned = Stark("Ned")
+    print(Ned.__dict__)
+    print(Ned.is_alive)
+    Ned.die()
+    print(Ned.is_alive)
+    print(Ned.__doc__)
+    print(Ned.__init__.__doc__)
+    print(Ned.die.__doc__)
+    print("---")
+    Lyanna = Stark("Lyanna", False)
+    print(Lyanna.__dict__)
+
+
+if __name__ == "__main__":
+    main()
