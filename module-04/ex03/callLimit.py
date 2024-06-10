6363135364b5fc47b0cb21748b4771615dd10484
Manuel SHARIFI <manuel.sharifi@gmail.com>
2024-06-10 15:54:42 +0200

    [FINISH] module-04

---
+++ module-04/ex03/callLimit.py
@@ -0,0 +1,40 @@
+def callLimit(limit: int):
+    """ Decorator set the limits the number
+        of times a function can be called."""
+    count = 0
+
+    def callLimiter(function):
+        """ Real decorator that limits the number of times"""
+
+        def limit_function(*args: any, **kwargs: any):
+            """ Inner function """
+            nonlocal count
+            count += 1
+            if count > limit:
+                return (print(f"Error: function {function.__name__}\
+                 at " f"{hex(id(function))} call too many times"))
+            return (function(*args, **kwargs))
+        return (limit_function)
+    return (callLimiter)
+
+
+def main():
+    try:
+        @callLimit(3)
+        def f():
+            print("f()")
+
+        @callLimit(1)
+        def g():
+            print("g()")
+        for i in range(3):
+            f()
+            g()
+    except Exception as e:
+        print(type(e).__name__ + ":", e)
+        return (1)
+    return (0)
+
+
+if __name__ == "__main__":
+    main()
